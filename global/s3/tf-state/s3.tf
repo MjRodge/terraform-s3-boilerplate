915a644331485e4c5a6753c0198f3273a619211c
mjrod <michael@1mrkt.to>
2022-06-06 19:53:35 -0400

    updated deprecated versioning syntax

--- global/s3/tf-state/s3.tf
+++ global/s3/tf-state/s3.tf
@@ -1,10 +1,7 @@
 # Configure an S3 bucket resource to hold application state files
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "<ENTER-A-GLOBALLY-UNIQUE-BUCKET-NAME-HERE>"  
-  # Add bucket versioning for state rollback
-  versioning {
-    enabled = true
-  }
+
   # Enable server-side encryption by default
   server_side_encryption_configuration {
     rule {
@@ -12,5 +9,13 @@ resource "aws_s3_bucket" "terraform_state" {
         sse_algorithm = "AES256"
       }
     }
+  }
+}
+
+  # Add bucket versioning for state rollback
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
