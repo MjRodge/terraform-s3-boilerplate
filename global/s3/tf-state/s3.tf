49dbedb8d382811bdbd87aa58d1103bbb59a61ac
mjrod <michael@1mrkt.to>
2022-06-06 20:08:17 -0400

    forgot to remove deprecated encryption code

--- global/s3/tf-state/s3.tf
+++ global/s3/tf-state/s3.tf
@@ -1,18 +1,9 @@
 # Configure an S3 bucket resource to hold application state files
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "<ENTER-A-GLOBALLY-UNIQUE-BUCKET-NAME-HERE>"  
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
-  # Add bucket versioning for state rollback
+# Add bucket versioning for state rollback
 resource "aws_s3_bucket_versioning" "state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
