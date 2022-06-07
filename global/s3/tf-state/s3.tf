91ba82abf778ef7d0e39456855ff41110b7519b5
mjrod <michael@1mrkt.to>
2022-06-06 20:04:28 -0400

    renoved deprecated code for s3 encryption

--- global/s3/tf-state/s3.tf
+++ global/s3/tf-state/s3.tf
@@ -19,3 +19,13 @@ resource "aws_s3_bucket_versioning" "state_versioning" {
     status = "Enabled"
   }
 }
+
+# Add bucket encryption to hide sensitive state data
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
